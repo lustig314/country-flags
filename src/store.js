ad5987819dc0186686c4c272fb87aa032563c229
lustig314 <60037706+lustig314@users.noreply.github.com>
2022-06-28 19:38:57 +0300

    first commit

---
+++ src/store.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { configureStore } from "@reduxjs/toolkit";
+
+import * as api from './config';
+import { countryReducer } from "./features/countries/countries-slice";
+import { controlsReducer } from "./features/controls/controls-slice";
+import { themeReducer } from "./features/theme/theme-slice";
+import { detailsReducer } from "./features/details/details-slice";
+
+export const store = configureStore({
+    reducer:{
+        theme: themeReducer,
+        controls: controlsReducer,
+        countries: countryReducer,
+        details: detailsReducer
+    },
+    devTools: true,
+    middleware: getDefaultMiddleware => getDefaultMiddleware({
+        thunk: {
+            extraArgument: {
+                client: axios,
+                api
+            }
+        },
+        serializableCheck: false,
+    })
+})
